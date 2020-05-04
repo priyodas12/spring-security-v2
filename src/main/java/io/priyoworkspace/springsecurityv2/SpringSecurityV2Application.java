30dd1c8b5aa60110e118993f2818b8d746dd1740
Priyobrato Das <priyodas12@gmail.com>
2020-05-05 00:56:47 +0530

    Initial commit

---
+++ src/main/java/io/priyoworkspace/springsecurityv2/SpringSecurityV2Application.java
@@ -0,0 +1,13 @@
+package io.priyoworkspace.springsecurityv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/priyoworkspace/springsecurityv2/SpringSecurityV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.priyoworkspace.springsecurityv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
