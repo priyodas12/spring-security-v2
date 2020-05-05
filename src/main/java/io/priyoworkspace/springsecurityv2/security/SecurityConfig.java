ebb6d544198c76916651f9e2faadd52b1a634893
Priyobrato Das <priyodas12@gmail.com>
2020-05-06 01:43:31 +0530

    customer user

--- src/main/java/io/priyoworkspace/springsecurityv2/security/SecurityConfig.java
+++ src/main/java/io/priyoworkspace/springsecurityv2/security/SecurityConfig.java
@@ -1,14 +1,29 @@
 package io.priyoworkspace.springsecurityv2.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //custom authentication details
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails userDetails= User.builder().username("priyo").password("####123").roles("dev").build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,5 +37,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+
+
+
+
     }
 }
