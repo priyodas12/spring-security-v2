44e6a45e62781917a92487b1bdcb3b00f0549f7d
Priyobrato Das <priyodas12@gmail.com>
2020-05-06 00:30:22 +0530

    security added

---
+++ src/main/java/io/priyoworkspace/springsecurityv2/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package io.priyoworkspace.springsecurityv2.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //any request coming through any controller end point to be authenticated.
+        //browser based authentication
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
